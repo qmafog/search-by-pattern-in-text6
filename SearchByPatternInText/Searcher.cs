4d00ae5ec85868664a6b3b25c919d0eb161d84b8
Alexander <154324-alexander-rykov@users.noreply.gitlab.com>
2022-02-09 23:25:32 +0300

    Add solution configuration files: editor configs, stylecop settings, style ruleset, readme. Add projects with unit tests.

---
+++ SearchByPatternInText/Searcher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SearchByPatternInText
+{
+    public static class Searcher
+    {
+        /// <summary>
+        /// Searches the pattern string inside the text and collects information about all hit positions in the order they appear.
+        /// </summary>
+        /// <param name="text">Source text.</param>
+        /// <param name="pattern">Source pattern text.</param>
+        /// <param name="overlap">Flag to overlap:
+        /// if overlap flag is true collect every position overlapping included,
+        /// if false no overlapping is allowed (next search behind).</param>
+        /// <returns>List of positions of occurrence of the pattern string in the text, if any and empty otherwise.</returns>
+        /// <exception cref="ArgumentException">Thrown if text or pattern is null.</exception>
+        public static int[] SearchPatternString(this string? text, string? pattern, bool overlap)
+        {
+            throw new NotImplementedException("You need to implement this method.");
+        }
+    }
+}
